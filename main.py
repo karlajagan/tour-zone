5bbccd66a2d5674c2fc0cd7390f65c058839df46
karlajagan <karla_jagannathan@hotmail.com>
2018-01-21 04:51:16 -0500

    added html's and modified main to include different users and login and registration

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogzpw@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'fsfsdfsdfsd'
@@ -13,10 +14,71 @@ class Blog(db.Model):
     blog_id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(100))
     blog = db.Column(db.String(300))
+    date_stamp = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-    def __init__(self, blog_title, blog):
+    def __init__(self, blog_title, blog, date_stamp, owner):
         self.blog_title = blog_title
         self.blog = blog
+        self.date_stamp = date_stamp
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(50))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login') 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash('logged in')
+            return redirect('/blog')
+        else:
+            flash('User password is incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        existing_user = User.query.filter_by(email=email).first()
+        
+        if not existing_user:
+            new_user = User(email,password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/blog')
+        else:
+            return '<h1>Duplicate user </h1>'
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/login')
         
 @app.route('/blog', methods=['GET'])
 def index():
@@ -24,7 +86,7 @@ def index():
     blog_id = request.args.get('blog_id', '')
     if blog_id == "":
         blogs = Blog.query.order_by(Blog.blog_id.desc()).all()
-        return render_template('display_blogs.html', title="Build a Blog Project", 
+        return render_template('display_blogs.html', title="The Best Blogs Online", 
             blogs=blogs)
     else:
         blog_id = int(blog_id)
@@ -43,6 +105,8 @@ def add_blog():
 @app.route('/newpost', methods=['POST'])
 def added_blog():
 
+    owner = User.query.filter_by(email=session['email']).first()
+    date_stamp = datetime.datetime.now()
     blog_title = request.form['blog_title']
     blog = request.form['blog']
     blog_error = ""
@@ -57,7 +121,7 @@ def added_blog():
             blog_error = blog_error )
         
     else:    
-        new_blog = Blog(blog_title,blog)
+        new_blog = Blog(blog_title, blog, date_stamp, owner)
         db.session.add(new_blog)
         db.session.commit()
 
