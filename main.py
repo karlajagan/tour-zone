88652a0c0d8d17804977a724765349b6b978aca8
karlajagan <karla_jagannathan@hotmail.com>
2018-06-10 11:14:18 -0400

    to generate the initial tour program

--- main.py
+++ main.py
@@ -5,11 +5,40 @@ import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogzpw@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tour-zone:tourzonepw@localhost:3306/tour-zone'
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = 'fsfsdfsdfsd'
 
-from model import db, Blog, User 
+#from model import db, Blog, User 
+
+db = SQLAlchemy(app)
+
+class Tour(db.Model):
+
+    tour_id = db.Column(db.Integer, primary_key=True)
+    tour = db.Column(db.String(100))
+    attraction = db.Column(db.String(500))
+    date_stamp = db.Column(db.DateTime)
+    provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'))
+    
+    def __init__(self, tour, attraction, date_stamp, provider):
+        self.tour = tour
+        self.attraction = attraction
+        self.date_stamp = date_stamp
+        self.provider = provider
+
+class Provider(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    tours = db.relationship('Tour', backref='provider')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
 def is_space(word):
     if ' ' in word:
@@ -25,8 +54,8 @@ def validate_field(field,text):
         error = text+"should not contain spaces"
         field = ""
     else:
-        if len(field) <= 3 or len(field) >= 20:
-            error = text+"must have between 3 and 20 characters"
+        if len(field) <= 3 or len(field) >= 40:
+            error = text+"must have between 3 and 40 characters"
             field = ""
     return error, field   
 
@@ -39,7 +68,7 @@ def validate_email_re(field):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register', 'index', 'blog_list']
+    allowed_routes = ['login', 'register', 'index', 'tour_list']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login') 
 
@@ -48,17 +77,17 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
+        provider = Provider.query.filter_by(username=username).first()
+        if provider and provider.password == password:
             session['username'] = username
-            return redirect('/blog')
+            return redirect('/tour')
         else:
-            flash('User password is incorrect, or user does not exist', 'error')
+            flash('Provider password is incorrect, or provider does not exist', 'error')
 
     if session.get('username') is not None:
         del session['username']
 
-    return render_template('login.html', title = "Blog Login" )
+    return render_template('login.html', title = "Provider Login" )
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -76,9 +105,9 @@ def register():
         validate_error = ''
         email_error = ''
 
-        existing_user = User.query.filter_by(username=username).first()
+        existing_provider = Provider.query.filter_by(username=username).first()
         
-        if not existing_user:
+        if not existing_provider:
 
             username_error, username = validate_field(username,"Username ")
     
@@ -99,8 +128,8 @@ def register():
                         email = ""
             if not username_error and not password_error and \
                 not validate_error and not email_error:                
-                new_user = User(username,email,password)
-                db.session.add(new_user)
+                new_provider = Provider(username,email,password)
+                db.session.add(new_provider)
                 db.session.commit()
                 password = ""
                 val_password = ""
@@ -116,7 +145,7 @@ def register():
                     email = email)   
             
         else:
-            return '<h1>Duplicate user </h1>'
+            return '<h1>Duplicate Provider </h1>'
 
     return render_template('register.html', 
                     title="Register", username_error = '',
@@ -137,13 +166,13 @@ def index():
         flash('logged in as '+session['username'])
     else:
         flash('not logged in') 
-    users = User.query.all()
-    return render_template("display_users.html", title = "Blog authors", users=users)
+    providers = Provider.query.all()
+    return render_template("display_providers.html", title = "Providers", providers=providers)
         
-@app.route('/blog', methods=['GET', 'POST'])
+@app.route('/tour', methods=['GET', 'POST'])
 def blog_list():
     #add all the blogs from a specific author
-    blog_id = request.args.get('blog_id', '')
+    tour_id = request.args.get('tour_id', '')
  
     username = ''
     if session.get('username') is not None:
@@ -151,57 +180,57 @@ def blog_list():
     else:
         flash('not logged in')       
         # username = session['username']
-    username_arg = request.args.get('user', '')
+    username_arg = request.args.get('provider', '')
     if username_arg != '':
         username = username_arg
 
     if username != '':
-        owner = User.query.filter_by(username=username).first()
-        blogs = Blog.query.order_by(Blog.blog_id.desc()).filter_by(owner=owner).all()
-        return render_template('display_blogs.html', title="The Best Blogs Online", 
-            blogs=blogs)
-    if blog_id == '':
-        blogs = Blog.query.order_by(Blog.blog_id.desc()).all()
-        return render_template('display_blogs.html', title="The Best Blogs Online", 
-            blogs=blogs)        
+        provider = Provider.query.filter_by(username=username).first()
+        tours = Tour.query.order_by(Tour.tour_id.desc()).filter_by(provider=provider).all()
+        return render_template('display_tours.html', title="The Best Tours Online", 
+            tours=tours)
+    if tour_id == '':
+        tours = Tour.query.order_by(Tour.tour_id.desc()).all()
+        return render_template('display_tours.html', title="The Best Tours Online", 
+            tours=tours)        
     else:
-        blog_id = int(blog_id)
-        blog = Blog.query.get(blog_id)
-        return render_template('display_blog.html', title=blog.blog_title, 
-            blog=blog.blog)
+        tour_id = int(tour_id)
+        tour = Tour.query.get(tour_id)
+        return render_template('display_tour.html', tour=tour.tour, 
+            attractions=tour.attraction)
     
 
 @app.route('/newpost', methods=['GET'])
-def add_blog():
+def add_tour():
     flash('logged in as '+session['username'])
-    return render_template('add-a-blog.html', title="Add a Blog", 
-        blog_title="" , blog="" , title_error="" , blog_error="")
+    return render_template('add-a-tour.html', title="Add a Tour", 
+        tour="" , attraction="" , tour_error="" , attraction_error="")
 
 
 @app.route('/newpost', methods=['POST'])
-def added_blog():
+def added_tour():
 
-    owner = User.query.filter_by(username=session['username']).first()
+    provider = Provider.query.filter_by(username=session['username']).first()
     date_stamp = datetime.datetime.now()
-    blog_title = request.form['blog_title']
-    blog = request.form['blog']
-    blog_error = ""
-    title_error = ""
-    if blog_title == "" or blog == "":
-        if blog_title == "":
-            title_error = "Please fill in the title"
-        if blog == "":
-            blog_error = "Please fill in the blog"
-        return render_template('add-a-blog.html',title="Add a Blog", 
-            blog_title = blog_title, blog = blog, title_error = title_error, 
-            blog_error = blog_error )
+    tour = request.form['tour']
+    attraction = request.form['attraction']
+    tour_error = ""
+    attraction_error = ""
+    if tour == "" or attraction == "":
+        if tour == "":
+            tour_error = "Please fill in the name of the tour"
+        if attraction == "":
+            attraction_error = "Please fill in the attractions covered"
+        return render_template('add-a-tour.html',title="Add a Tour", 
+            tour = tour, attraction = attraction, tour_error = tour_error, 
+            attraction_error = attraction_error )
         
     else:    
-        new_blog = Blog(blog_title, blog, date_stamp, owner)
-        db.session.add(new_blog)
+        new_tour = Tour(tour, attraction, date_stamp, provider)
+        db.session.add(new_tour)
         db.session.commit()
 
-        return redirect('/blog?blog_id='+str(new_blog.blog_id))
+        return redirect('/tour?tour_id='+str(new_tour.tour_id))
 
 if __name__ == '__main__':
     app.run()
